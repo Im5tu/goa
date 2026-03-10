263bbd9c1d56de1f256d19f8d2bd668b39c10a44
Stuart Blackler <Im5tu@users.noreply.github.com>
2026-03-10 21:45:30 +0000

    fix: Throw DynamoPaginationException on pagination errors (#71)

---
+++ src/Clients/Goa.Clients.Dynamo/Exceptions/DynamoPaginationException.cs
@@ -0,0 +1,36 @@
+using ErrorOr;
+
+namespace Goa.Clients.Dynamo.Exceptions;
+
+/// <summary>
+/// Exception thrown when an auto-pagination operation encounters an error during a page request.
+/// This prevents silent truncation of results in methods like QueryAllAsync, ScanAllAsync, and BatchGetAllAsync.
+/// </summary>
+public class DynamoPaginationException : Exception
+{
+    /// <summary>
+    /// Gets the underlying error that caused the pagination failure.
+    /// </summary>
+    public Error Error { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DynamoPaginationException"/> class.
+    /// </summary>
+    /// <param name="error">The error that caused the pagination failure.</param>
+    public DynamoPaginationException(Error error)
+        : base($"A DynamoDB pagination request failed with error '{error.Code}': {error.Description}")
+    {
+        Error = error;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DynamoPaginationException"/> class with an inner exception.
+    /// </summary>
+    /// <param name="error">The error that caused the pagination failure.</param>
+    /// <param name="innerException">The inner exception.</param>
+    public DynamoPaginationException(Error error, Exception innerException)
+        : base($"A DynamoDB pagination request failed with error '{error.Code}': {error.Description}", innerException)
+    {
+        Error = error;
+    }
+}
